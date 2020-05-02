916609f0cf23783dc5d75aed6ba48634ba8e014e
ysveshnikov <ysveshnikov@ivi.ru>
2020-05-02 20:00:12 +0300

    Исправлены отступы

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 import program from 'commander';
-import getDiff from "../src/index.js";
+import getDiff from '../src/index.js';
 
 program
-    .version('0.0.1')
-    .description('Compares two configuration files and shows a difference.')
-    .arguments('<filepath1> <filepath2>')
-    .action((filepath1, filepath2) => {
-        const diff = getDiff(filepath1, filepath2);
-        console.log(diff);
-    })
-    .option('-f, --format [type]', 'output format')
-    .parse(process.argv);
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    const diff = getDiff(filepath1, filepath2);
+    console.log(diff);
+  })
+  .option('-f, --format [type]', 'output format')
+  .parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -2,40 +2,40 @@ import fs from 'fs';
 import path from 'path';
 
 const compareObj = (src1, src2) => {
-    const result = {};
+  const result = {};
 
-    for (const key in src1) {
-        if (key in src2) {
-            if (src1[key] === src2[key]) {
-                result[`  ${key}`] = src1[key];
-            } else {
-                result[`+ ${key}`] = src2[key];
-                result[`- ${key}`] = src1[key];
-            }
-        } else {
-            result[`- ${key}`] = src1[key];
-        }
+  for (const key in src1) {
+    if (key in src2) {
+      if (src1[key] === src2[key]) {
+        result[`  ${key}`] = src1[key];
+      } else {
+        result[`+ ${key}`] = src2[key];
+        result[`- ${key}`] = src1[key];
+      }
+    } else {
+      result[`- ${key}`] = src1[key];
     }
+  }
 
-    for (const key in src2) {
-        if (!(key in src1)) {
-            result[`+ ${key}`] = src2[key];
-        }
+  for (const key in src2) {
+    if (!(key in src1)) {
+      result[`+ ${key}`] = src2[key];
     }
+  }
 
-    return result;
+  return result;
 };
 
 const getDiff = (path1, path2) => {
-    const file1 = fs.readFileSync(path.resolve(process.cwd(), path1));
-    const file2 = fs.readFileSync(path.resolve(process.cwd(), path2));
+  const file1 = fs.readFileSync(path.resolve(process.cwd(), path1));
+  const file2 = fs.readFileSync(path.resolve(process.cwd(), path2));
 
-    const serializeFile1 = JSON.parse(file1);
-    const serializeFile2 = JSON.parse(file2);
+  const serializeFile1 = JSON.parse(file1);
+  const serializeFile2 = JSON.parse(file2);
 
-    const result = JSON.stringify(compareObj(serializeFile1, serializeFile2), null, ' ');
+  const result = JSON.stringify(compareObj(serializeFile1, serializeFile2), null, ' ');
 
-    return result;
+  return result;
 };
 
 export default getDiff;
